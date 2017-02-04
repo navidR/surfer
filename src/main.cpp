03eda41f2fa2664b1ed1b02c90b67a684da81d3b
Navid Rahimi <rahimi.nv@gmail.com>
2017-02-04 18:57:27 +0330

    arg error

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,9 @@ int main(int argc, char ** argv)
         ("download_location", po::value<string>(), "Path to download location")
     ;
 
-    //po::variables_map vm;
-    //po::store(po::parse_command_line(argc, argv, desc), vm);
-    //po::notify(vm);
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
 
     try {
         po::notify(vm);
@@ -38,44 +38,44 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    // if (vm.count("help")) {
-    //     cout << desc << endl;
-    //     return 1;
-    // }
+    if (vm.count("help")) {
+        cout << desc << endl;
+        return 1;
+    }
 
-    // if (vm.count("start")) {
-    //     cout << "Start number in url" 
-    //     << vm["start"].as<int>() << endl;
-    //     start = vm["start"].as<int>();
-    // } else {
-    //     cout << "start number should be provided." << endl;
-    //     return 1;
-    // }
+    if (vm.count("start")) {
+        cout << "Start number in url" 
+        << vm["start"].as<int>() << endl;
+        start = vm["start"].as<int>();
+    } else {
+        cout << "start number should be provided." << endl;
+        return 1;
+    }
 
-    // if (vm.count("end")) {
-    //     cout << "end number in url" 
-    //     << vm["end"].as<int>() << endl;
-    //     end = vm["end"].as<int>();
-    // } else {
-    //     cout << "end number should be provided." << endl;
-    //     return 1;
-    // }
+    if (vm.count("end")) {
+        cout << "end number in url" 
+        << vm["end"].as<int>() << endl;
+        end = vm["end"].as<int>();
+    } else {
+        cout << "end number should be provided." << endl;
+        return 1;
+    }
 
-    // if(vm.count("download")) {
-    //     download = true;
-    //     if(vm.count("download_location")) {
-    //         cout << "Download location is : " << download_location << endl;
-    //         download_location = vm["download_location"].as<string>();
-    //     } else {
-    //         cout << "download location should be provided." << endl;
-    //         return 1;
-    //     }
-    // } else {
-    //     if(vm.count("download_location")) {
-    //         cout << "download flag should provided before using download_location." << endl;
-    //         return 1;
-    //     } 
-    // }
+    if(vm.count("download")) {
+        download = true;
+        if(vm.count("download_location")) {
+            cout << "Download location is : " << download_location << endl;
+            download_location = vm["download_location"].as<string>();
+        } else {
+            cout << "download location should be provided." << endl;
+            return 1;
+        }
+    } else {
+        if(vm.count("download_location")) {
+            cout << "download flag should provided before using download_location." << endl;
+            return 1;
+        } 
+    }
 
 
     return 0;
